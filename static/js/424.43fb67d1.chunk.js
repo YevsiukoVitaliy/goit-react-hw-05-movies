ccf78180cdfb8bf55a65d8b0c76b8047979a92a8
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2022-09-09 20:25:33 +0000

    Deploying to gh-pages from @ YevsiukoVitaliy/goit-react-hw-05-movies@5641e1d94f89b7a973813acb259b490277c0da7a 🚀

--- static/js/424.43fb67d1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[424],{424:function(e,t,n){n.r(t);var u=n(791),o=n(871),c=n(184);t.default=function(){var e=(0,o.s0)();return(0,u.useEffect)((function(){setTimeout((function(){e(-1)}),1e3)}),[e]),(0,c.jsx)("h2",{children:"Sorry,not found"})}}}]);
-//# sourceMappingURL=424.43fb67d1.chunk.js.map
---
+++ static/js/424.7e3f7e52.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[424],{424:function(e,t,n){n.r(t);var u=n(791),o=n(871),c=n(184);t.default=function(){var e=(0,o.s0)();return(0,u.useEffect)((function(){setTimeout((function(){e("/")}),1e3)}),[e]),(0,c.jsx)("h2",{children:"Sorry,not found"})}}}]);
+//# sourceMappingURL=424.7e3f7e52.chunk.js.map
